9e913e247871810f601ef8edc9fbc412ec9a5d16
mm-news <105084@mhps.tp.edu.tw>
2022-02-19 15:34:16 +0800

    爬牆功能
    使實體可以爬牆

---
+++ java/edu/mod/exam_mod/WallClimb.java
@@ -0,0 +1,14 @@
+package edu.mod.exam_mod;
+
+import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class WallClimb {
+    @SubscribeEvent
+    public void climbWall(LivingUpdateEvent event) {
+        if (!event.getEntity().horizontalCollision) {
+            return;
+        }
+        event.getEntity().setDeltaMovement(0F, 0.5F, 0F);
+    }
+}
