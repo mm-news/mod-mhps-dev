7bf1814393349fa9828f6fc67ad032f0d19f3f4a
mm-news <105084@mhps.tp.edu.tw>
2022-03-15 13:22:20 +0800

    將爆炸改為限於苦力怕

---
+++ java/edu/mod/exam_mod/ExplodeWhenCreeperDie.java
@@ -0,0 +1,18 @@
+package edu.mod.exam_mod;
+
+import net.minecraft.world.entity.monster.Creeper;
+import net.minecraft.world.level.Explosion;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class ExplodeWhenCreeperDie {
+    
+    @SubscribeEvent
+    public void explodeWhenEntityDie(LivingDeathEvent event) {
+
+
+        if(event.getEntity() instanceof Creeper) {
+            event.getEntity().level.explode(event.getEntity(), event.getEntity().getX(), event.getEntity().getY(), event.getEntity().getZ(), 1F, Explosion.BlockInteraction.DESTROY);
+        }
+    }
+}
