e62f8237d29ccf9e1abadbdbc74529f3609e648a
mm-news <105084@mhps.tp.edu.tw>
2022-03-21 20:42:23 +0800

    降落傘機制合理化
    每當玩家使用降落傘時，
    就將其掉落距離重置，
    使玩家即使在最後一刻放開 shift 鍵也不會受到掉落傷害。

--- java/edu/mod/exam_mod/SlowFalling.java
+++ java/edu/mod/exam_mod/SlowFalling.java
@@ -1,35 +1,20 @@
 package edu.mod.exam_mod;
 
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.event.TickEvent.PlayerTickEvent;
-import net.minecraftforge.event.entity.living.LivingFallEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 public class SlowFalling {
     @SubscribeEvent
-    public void parachutePartI(PlayerTickEvent event){
+    public void parachute(PlayerTickEvent event){
     if(!(event.player.isShiftKeyDown())){
         return;
     }
 
+    event.player.resetFallDistance();
+
     Vec3 playerMovement = event.player.getDeltaMovement();
 
     event.player.setDeltaMovement(playerMovement.x, -0.5F, playerMovement.z);
-    }
-
-    @SubscribeEvent
-    public void parachutePartII(LivingFallEvent event){
-        if (!(event.getEntity() instanceof Player)){
-            return;
-        }
-
-        Player player = (Player)event.getEntity();
-
-        if (!player.isShiftKeyDown()) {
-            return;
-        }
-
-        event.setCanceled(true);
 
     }
 }
