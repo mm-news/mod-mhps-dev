5714a9abacc40c1d8b5ba6c3f6c95a0c8b01f1e8
mm-news <105084@mhps.tp.edu.tw>
2022-03-23 11:18:47 +0800

    自訂磚塊

---
+++ java/edu/mod/exam_mod/block/ExamBlock.java
@@ -0,0 +1,29 @@
+package edu.mod.exam_mod.block;
+
+import java.util.function.Supplier;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ExamBlock {
+    
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, "mod_mhps");
+
+    public void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+
+    private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block) {
+        
+    }
+
+}
