fbd056cab4ce28b0d0df643dd23f439caea3b39d
mm-news <105084@mhps.tp.edu.tw>
2022-03-23 08:34:42 +0800

    未完成

---
+++ java/edu/mod/exam_mod/ExampleBlock.java
@@ -0,0 +1,17 @@
+package edu.mod.exam_mod;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.material.Material;
+
+public class ExampleBlock extends Block{
+    
+    public ExampleBlock() {
+
+        //super(Blocks.STONE);
+        this.explosionResistance = 0F;
+        
+
+    }
+
+}
