bec28d3de5c9ea6889d1fb7c3fd9515bd4876cf5
mm-news <105084@mhps.tp.edu.tw>
2022-03-30 20:53:30 +0800

    爬牆手套 Part V
    使爬牆功能限制在手持爬牆手套的玩家

--- java/edu/mod/mod_mhps/WallClimb.java
+++ java/edu/mod/mod_mhps/WallClimb.java
@@ -1,5 +1,6 @@
 package edu.mod.mod_mhps;
 
+import edu.mod.mod_mhps.item.WallClimbingGlove;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -7,6 +8,11 @@ public class WallClimb {
 
     @SubscribeEvent
     public void climbWall(LivingUpdateEvent event) {
+
+        if (!event.getEntityLiving().isHolding(WallClimbingGlove.WALL_CLIMBING_GLOVE.get())) {
+            return;
+        }
+
         if (!event.getEntity().horizontalCollision) {
             return;
         }
