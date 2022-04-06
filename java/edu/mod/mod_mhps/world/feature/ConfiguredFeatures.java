e28125dd32b3b196cd01df012d5eb9d68354f401
mm-news <105084@mhps.tp.edu.tw>
2022-04-06 20:13:14 +0800

    刪掉不需要的內容

--- java/edu/mod/mod_mhps/world/feature/ConfiguredFeatures.java
+++ java/edu/mod/mod_mhps/world/feature/ConfiguredFeatures.java
@@ -1,41 +1,17 @@
 package edu.mod.mod_mhps.world.feature;
 
-import net.minecraft.core.Holder;
-import net.minecraft.data.worldgen.features.FeatureUtils;
-import net.minecraft.data.worldgen.features.OreFeatures;
-import net.minecraft.data.worldgen.placement.PlacementUtils;
-import net.minecraft.util.valueproviders.ConstantInt;
-import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
-import net.minecraft.world.level.levelgen.feature.Feature;
-import net.minecraft.world.level.levelgen.feature.WeightedPlacedFeature;
-import net.minecraft.world.level.levelgen.feature.configurations.*;
-import net.minecraft.world.level.levelgen.feature.featuresize.TwoLayersFeatureSize;
-import net.minecraft.world.level.levelgen.feature.foliageplacers.BlobFoliagePlacer;
-import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
-import net.minecraft.world.level.levelgen.feature.trunkplacers.StraightTrunkPlacer;
-import net.minecraft.world.level.levelgen.placement.PlacedFeature;
-
 import java.util.List;
 
 import edu.mod.mod_mhps.block.AntiGravityStone;
 import edu.mod.mod_mhps.block.RadioactiveBlock;
+import net.minecraft.core.Holder;
+import net.minecraft.data.worldgen.features.FeatureUtils;
+import net.minecraft.data.worldgen.features.OreFeatures;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
 
 public class ConfiguredFeatures {
-    public static final Holder<ConfiguredFeature<TreeConfiguration, ?>> EBONY_TREE =
-            FeatureUtils.register("ebony", Feature.TREE, new TreeConfiguration.TreeConfigurationBuilder(
-                    BlockStateProvider.simple(RadioactiveBlock.RADIOACTIVE_BLOCK.get()),
-                    new StraightTrunkPlacer(5, 6, 3),
-                    BlockStateProvider.simple(RadioactiveBlock.RADIOACTIVE_BLOCK.get()),
-                    new BlobFoliagePlacer(ConstantInt.of(2), ConstantInt.of(0), 4),
-                    new TwoLayersFeatureSize(1, 0, 2)).build());
-
-    public static final Holder<PlacedFeature> EBONY_CHECKED = PlacementUtils.register("ebony_checked", EBONY_TREE,
-                    PlacementUtils.filteredByBlockSurvival(RadioactiveBlock.RADIOACTIVE_BLOCK.get()));
-
-    public static final Holder<ConfiguredFeature<RandomFeatureConfiguration, ?>> EBONY_SPAWN =
-            FeatureUtils.register("ebony_spawn", Feature.RANDOM_SELECTOR,
-                    new RandomFeatureConfiguration(List.of(new WeightedPlacedFeature(EBONY_CHECKED,
-                            0.5F)), EBONY_CHECKED));
 
     public static final List<OreConfiguration.TargetBlockState> OVERWORLD_CITRINE_ORES = List.of(
             OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES, RadioactiveBlock.RADIOACTIVE_BLOCK.get().defaultBlockState()),
