38c142bf741514082ace563b37ce865fcea24c79
mm-news <105084@mhps.tp.edu.tw>
2022-04-06 11:05:31 +0800

    格式化 Import

--- java/edu/mod/mod_mhps/world/gen/OreGeneration.java
+++ java/edu/mod/mod_mhps/world/gen/OreGeneration.java
@@ -1,13 +1,12 @@
 package edu.mod.mod_mhps.world.gen;
 
+import java.util.List;
+
+import edu.mod.mod_mhps.world.feature.PlacedFeatures;
 import net.minecraft.core.Holder;
 import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
-
-import java.util.List;
-
-import edu.mod.mod_mhps.world.feature.PlacedFeatures;
 
 public class OreGeneration {
     public static void generateOres(final BiomeLoadingEvent event) {
@@ -15,5 +14,6 @@ public class OreGeneration {
                 event.getGeneration().getFeatures(GenerationStep.Decoration.UNDERGROUND_ORES);
 
         base.add(PlacedFeatures.CITRINE_ORE_PLACED);
+
     }
 }
