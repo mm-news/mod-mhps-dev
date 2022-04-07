c5c64f3939184d0b106627df40cbb0f1e1c539da
mm-news <105084@mhps.tp.edu.tw>
2022-04-07 09:31:34 +0800

    格式正常化

--- java/edu/mod/mod_mhps/SlowFalling.java
+++ java/edu/mod/mod_mhps/SlowFalling.java
@@ -1,20 +1,27 @@
 package edu.mod.mod_mhps;
 
+import edu.mod.mod_mhps.block.AntiGravityStone;
+import edu.mod.mod_mhps.item.AntiGravityParticles;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.event.TickEvent.PlayerTickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 public class SlowFalling {
     @SubscribeEvent
     public void parachute(PlayerTickEvent event){
-    if(!(event.player.isShiftKeyDown())){
-        return;
-    }
+    
+        if(!(event.player.isShiftKeyDown())){
+            return;
+        }
 
-    event.player.resetFallDistance();
+        if (!(event.player.isHolding(AntiGravityParticles.ANTI_GRAVITY_PARTICLES.get())) && !(event.player.isHolding(AntiGravityStone.ANTI_GRAVITY_STONE.get().asItem()))) {
+            return;
+        }
 
-    Vec3 playerMovement = event.player.getDeltaMovement();
+        event.player.resetFallDistance();
 
-    event.player.setDeltaMovement(playerMovement.x, -0.5F, playerMovement.z);
+        Vec3 playerMovement = event.player.getDeltaMovement();
+
+        event.player.setDeltaMovement(playerMovement.x, -0.5F, playerMovement.z);
 
     }
 }
