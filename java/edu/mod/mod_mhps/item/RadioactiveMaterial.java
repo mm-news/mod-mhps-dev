5de5aae5c7e9e7d8a2c41baa369407970b947cd4
mm-news <105084@mhps.tp.edu.tw>
2022-04-13 20:14:47 +0800

    讓放射性物質可以吃
    吃放射性物質時會給予
    
    - 2 飽食度 ( 1 個標記 )
    - 效果
    -- 機率 10%
    --- 失明
    --- 中毒
    --- 飽食
    --- 隱形
    --- 力量
    -- 機率 50%
    --- 沒有效果

--- java/edu/mod/mod_mhps/item/RadioactiveMaterial.java
+++ java/edu/mod/mod_mhps/item/RadioactiveMaterial.java
@@ -1,5 +1,8 @@
 package edu.mod.mod_mhps.item;
 
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -22,6 +25,27 @@ public class RadioactiveMaterial {
         () -> new Item(
             new Item.Properties().tab(
                 ItemGroup.MOD_MHPS
+            )
+            .food(
+                new FoodProperties
+                .Builder()
+                .nutrition(2)
+                .saturationMod(1.5F)
+                .effect(
+                    () -> new MobEffectInstance(MobEffects.POISON, 5*20), 0.1F
+                )
+                .effect(
+                    () -> new MobEffectInstance(MobEffects.BLINDNESS, 5*20), 0.1F
+                )
+                .effect(
+                    () -> new MobEffectInstance(MobEffects.SATURATION, 180*20), 0.1F
+                )
+                .effect(
+                    () -> new MobEffectInstance(MobEffects.DAMAGE_BOOST, 180*20), 0.1F
+                )
+                .effect(
+                    () -> new MobEffectInstance(MobEffects.INVISIBILITY, 100*20), 0.1F)
+                .build()
             )
         )
     );
