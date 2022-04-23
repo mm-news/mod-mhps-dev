17a54f44715eecda3ac8d5807cc22631b8d7d6ba
mm-news <105084@mhps.tp.edu.tw>
2022-04-23 16:40:31 +0800

    戴肥球Addons

--- java/edu/mod/mod_mhps/ExplodeWhenDieDie.java
+++ java/edu/mod/mod_mhps/ExplodeWhenDieDie.java
@@ -1,17 +1,17 @@
 package edu.mod.mod_mhps;
 
-import net.minecraft.world.entity.monster.Creeper;
+import edu.mod.mod_mhps.common.entity.DieBall;
 import net.minecraft.world.level.Explosion;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
-public class ExplodeWhenCreeperDie {
+public class ExplodeWhenDieDie {
     
     @SubscribeEvent
     public void explodeWhenEntityDie(LivingDeathEvent event) {
 
 
-        if(event.getEntity() instanceof Creeper) {
+        if(event.getEntity() instanceof DieBall) {
             event.getEntity().level.explode(event.getEntity(), event.getEntity().getX(), event.getEntity().getY(), event.getEntity().getZ(), 1F, Explosion.BlockInteraction.DESTROY);
         }
     }
